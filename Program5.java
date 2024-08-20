53151bbd48a2d277cad2ee47073772a17ed43606
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-08-20 15:25:23 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,9 +1,29 @@
 //import stuff here?
 
 //Your code here
-
+class Main
+{
+public static void main(String[] args)
+  {
+    double mil [] = {286, 412, 361, 161};
+    double gal [] = {9, 40, 18, 11};
+    System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:\n");
+    System.out.printf("Royale averaged " + "%.1f m/g\n\n", mil[0]/gal[0]);
+    System.out.printf("Koopa King averaged " + "%.1f m/g\n\n", mil[1]/gal[1]);
+    System.out.printf("Pipe Frame averaged " + "%.1f m/g\n\n", mil[2]/gal[2]);
+    System.out.printf("Badwagon averaged " + "%.1f m/g\n\n", mil[3]/gal[3]);
+  }
+}
 //Paste console output below:
 /*
+Mushroom Cup Prix Racer Average Miles/Per Gallon:
 
+Royale averaged 31.8 m/g
+
+Koopa King averaged 10.3 m/g
+
+Pipe Frame averaged 20.1 m/g
+
+Badwagon averaged 14.6 m/g
 
 */
